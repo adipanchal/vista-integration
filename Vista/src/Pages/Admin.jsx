b7085d829371cc5e2cc4593a627e0716314c0ba5
Aditya Panchal <adipanchal443@gmail.com>
2024-05-31 01:18:37 +0530

    misc

--- Vista/src/Pages/Admin.jsx
+++ Vista/src/Pages/Admin.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Admin() {
   return (
     <>
-      
+      <div className="admin-login w-full h-screen flex justify-center items-center bg-black">
+      <h2 className="text-2xl mb-10 text-primary italic text-center">Admin Panel is coming soon!</h2>
+      </div>
     </>
   )
 }
